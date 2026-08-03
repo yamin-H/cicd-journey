ca1af4bdbb46a79c776d62b9be76a2cb83ac0a1b
Yamin Hossain <yamindr3@gmail.com>
2026-08-03 09:19:35 +0600

    restructure pipeline into stages

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 def add(a, b):
     return a + b
 
+
 def subtract(a, b):
     return a - b
 
+
 def multiply(a, b):
-    return a + b  # bug: using + instead of *
+    return a * b
+
 
 print(add(3, 4))
